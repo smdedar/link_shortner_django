cb7cc720968802a079aac20f5b4218aae9aac52c
Dedar Alam <thededar@gmail.com>
2021-02-25 16:13:55 +0600

    initial complete

--- shortner/urls.py
+++ shortner/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.link_shortner, name='link_shortner'),
-    path('<str:slink>', views.link_redirect, name='link_redirect'),
+    path('<str:alias>', views.link_redirect, name='link_redirect'),
 ]
--- shortner/views.py
+++ shortner/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import redirect, render, HttpResponse
 from .forms import LinkForm
 from .models import Link
 
-# Create your views here.
+import string
+import random
+
+#Generate Random String
+def random_string():
+    # Length of string needed
+    N = 5
+    # With random.choices()
+    res = ''.join(random.choices(string.ascii_lowercase+string.digits, k=N))
+    return res
+
+# Long Link Convert to Short Link
 def link_shortner(request):
     form = LinkForm(request.POST or None)
 
@@ -10,7 +21,7 @@ def link_shortner(request):
         #Get Value From Input Form
         longLink = form.cleaned_data['longLink']
         #Here Random String Generator
-        alias = 'testsort1'
+        alias = random_string()
         #Short Link
         shortLink = 'http://127.0.0.1:8000/'+alias
         #Save The Short Link to Database
@@ -21,8 +32,11 @@ def link_shortner(request):
 
     return render(request, 'index.html', {'form':form})
 
-def link_redirect(request,slink):
-    link = Link.objects.get(shortLink=slink)
+#Link Redirect to Orginal Link
+def link_redirect(request, alias):
+    link = Link.objects.get(shortLink=alias)
     #return HttpResponse(link.longLink)
     return redirect(link.longLink)
+
+
 
